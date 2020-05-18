949caa11fe37d72da82b25c09df9ff89a3cca798
Bajram Bojku <bajrambojku@Bajrams-MBP.fios-router.home>
2020-05-18 14:06:19 -0500

    bearer

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
+
 # cdap_pipeline_uploader
 
-
 ## Function
+
 This will loop through a directory of .Json files exported from cdap and upload all files in that directory to the specified namespace.
 
 ### Use
 
-
-| Variable  	| Use                                                               
-|-----------	|-------------------------------------------------------------------
-| dir       	| Directory to pull files from                                      
-| namespace 	| Namespace to upload pipelines to                                  
-| host      	| Default is for cdap. Change to CDF instance url/api for CDF use 
+| Variable      | Use
+|-----------    |-------------------------------------------------------------------
+| dir           | Directory to pull files from
+| namespace     | Namespace to upload pipelines to
+| host          | Default is for cdap. Change to CDF instance url/api for CDF use
 | authToken     | Authtoken from gcloud auth for CDF use
 | trim          | Default is "-cdap-data-pipeline.json" for cdap
--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 	"strings"
 )
 
-var host string = "http://localhost:11015/v3"
-var namespace string = "default"
-var trim string = "-cdap-data-pipeline.json"
-
-//var authToken string = ""
-
 func main() {
+
+	var host string = "http://localhost:11015/v3"
+	var authToken string = ""
+	var namespace string = "default"
+	var trim string = "-cdap-data-pipeline.json"
 
 	//get a dir and the files that dir holds
 	dir := "/Users/bajrambojku/pipelines"
+
 	items, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
@@ -32,7 +32,7 @@ func main() {
 	for _, item := range items {
 		items, _ := ioutil.ReadFile(dir + "/" + item.Name())
 		if strings.HasSuffix(item.Name(), ".json") {
-			go deployPipeline(items, item.Name(), host, namespace, trim, c)
+			go deployPipeline(items, item.Name(), host, namespace, trim, c, authToken)
 		}
 	}
 
@@ -42,16 +42,16 @@ func main() {
 	}
 }
 
-func deployPipeline(jsonData []byte, fileName string, host string, namespace string, trim string, c chan string) {
+func deployPipeline(jsonData []byte, fileName string, host string, namespace string, trim string, c chan string, authToken string) {
 
 	//exported pipelines come with -cdap-data-pipeline.json suffix we only want the name to call the pipline
 	fileName = strings.TrimSuffix(fileName, trim)
 
-	// var bearer = "Bearer" + authToken
+	var bearer = "Bearer" + authToken
 
 	request, _ := http.NewRequest("PUT", host+"/namespaces/"+namespace+"/apps/"+fileName, bytes.NewBuffer(jsonData))
 	request.Header.Set("Content-Type", "application/json")
-	// request.Header.Add("Authorization", bearer)
+	request.Header.Add("Authorization", bearer)
 	client := &http.Client{}
 	response, err := client.Do(request)
 	data, _ := ioutil.ReadAll(response.Body)
